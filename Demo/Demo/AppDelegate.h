dd4eb0b0a5a1a249f080efabab6fc311ff2ad806
aaasun <chinatns@126.com>
2015-02-06 15:49:50 +0800

    new file

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by front on 15/2/6.
+//  Copyright (c) 2015年 front. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
